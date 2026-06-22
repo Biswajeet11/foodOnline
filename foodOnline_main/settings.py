e90a6f1ce0f13d755c764cf284b1fbd0e7074520
Biswajeet Gope <biswajeetgp25@gmail.com>
2026-06-22 18:03:11 +0530

    added google location api

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'accounts.context_processor.get_vendor',
+                'accounts.context_processor.get_google_api',
             ],
         },
     },
@@ -148,3 +150,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <django.foodonline@gmail.com>'
+
+GOOGLE_API_KEY = config('GOOGLE_API_KEY')
